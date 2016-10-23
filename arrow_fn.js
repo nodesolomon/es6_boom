5020b440a92282d97370900c44ea6a54a65900a4
Solomon Ahmad <solomon.min.js@gmail.com>
2016-10-23 17:53:40 +0100

    Arrow Mayhem

---
+++ arrow_fn.js
@@ -0,0 +1,8 @@
+var values_es6 = [0, 3, 2, 5, 7, 4, 8, 1];
+
+// ES6
+values_es6.sort((value1, value2) => value1 - value2);
+
+var sayHey = name => "Hey " + name;
+assert(sayHey("Solomon") === "Hey Solomon", "Says Hello To Me!");
+
